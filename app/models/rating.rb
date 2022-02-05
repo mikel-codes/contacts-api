4754d83b6afae0ab889ed366dc0cb93941def131
smeez <gatez@yahoo.com>
2022-02-05 17:44:08 +0000

    Made inclusion line neat

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,5 @@
 class Rating < ApplicationRecord
   belongs_to :contact
   validates :contact, presence: true
-  validates :value, inclusion: {in: 1..5 }
+  validates :value, inclusion: { in: 1..5 }
 end
